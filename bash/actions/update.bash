54ac6b7ca866a33823327209d9e2a3423ae39525
José Martínez <marquevjosman@gmail.com>
2025-02-20 21:34:47 -0600

    update

--- bash/actions/update.bash
+++ bash/actions/update.bash
@@ -3,7 +3,11 @@ source bash/modules/try.bash
 try bash/actions/format.bash
 function commit {
   echo "=> Commit.sh"
+  source bash/modules/error.bash
   try git add .
-  try git commit -m "update"
+  if ! git commit -m "update"
+  then
+    error "commit failed"
+  fi
 }
 time commit "$@"
